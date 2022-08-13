6db4dae80b3d8089582cc4e6325788427fa4078a
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2022-08-13 14:23:04 +0200

    Test from fork

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@ int main(int argc, char **argv)
 {
     auto test = new std::string;  // :)
     std::cout << "Hello xD\n";
+    auto test69 = new std::string;
     auto test2 = new std::string;  // :)
     auto test3 = new std::string;  // :)
     auto test4 = new std::string;  // :)
