76fd55799da420e947f716bce33da4cb0d4aa295
Felanbird <41973452+Felanbird@users.noreply.github.com>
2022-08-13 08:26:18 -0400

    do stuff

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,9 @@ int main(int argc, char **argv)
 {
     auto test = new std::string;  // :)
     std::cout << "Hello xD\n";
-    auto test2 = new std::string;  // :)
+    auto test12 = new std::string;  // :)
     auto test3 = new std::string;  // :)
     auto test4 = new std::string;  // :)
+	auto test5 = new std::string;
     return 0;
 }
